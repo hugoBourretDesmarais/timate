e453ef034fddb86e82536ad5bd3e2cd596b61b68
Hugo Bourret-Desmarais <hugo.bourret.desmarais@gmail.com>
2018-04-29 14:36:11 -0400

    Experimentation of histogram with d3js

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterContentInit } from '@angular/core';
+import { Component, AfterContentInit, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { CommunicationService } from './communication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,65 @@ import * as d3 from 'd3';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements AfterContentInit {
+export class AppComponent implements OnInit, AfterContentInit {
+
+  constructor(private communicationService: CommunicationService) { }
+
+  ngOnInit(): void {
+    var w = 500;
+    var h = 200;
+    var padding = 5;
+    var dataset = [5,10,15,20,25];
+    var svg = d3.select("svg")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    var min = 0;
+    var max = d3.max(dataset, function(d){return d});
+    var yScale = d3.scaleLinear().domain([min, max]).range([5,h-10]);
+    var xScale = d3.scaleLinear().domain([0, ]).
+
+    var yAxis = d3.axisLeft(yScale).ticks(5);
+    var axis = svg.append("g").call(yAxis)
+                              .attr("class", "axis")
+                              .attr("transform", "translate(" + 20 + ", 0)");
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+          .attr("x", function(d,i) {
+            return (i * (w / dataset.length));
+          })
+          .attr("y", function(d) {
+            return h - yScale(d);
+          })
+          .attr("width", w / dataset.length - padding)
+          .attr("height", function(d){
+            return yScale(d);
+          })
+          .attr("fill", function(d){
+            return "rgb(" + (d * 10) + ", 0, 0)";
+          })
+          .on("mouseover", function(d) {
+            svg.append("text")
+              .text(d)
+              .attr("text-anchor", "middle")
+              .attr("x", parseFloat(d3.select(this).attr("x")) + parseFloat(d3.select(this).attr("width"))/2)
+              .attr("y", parseFloat(d3.select(this).attr("y")) + 12)
+              .attr("font-family", "sans-serif")
+              .attr("font-size", 12)
+              .attr("fill", "#ffffff")
+             . attr("id", "tooltip")
+          })
+          .on("mouseout", function() {
+            d3.select("#tooltip").remove();
+          })
+    
+
+        
+  }
   title = 'app';
 
   radius = 10;
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CommunicationService } from './communication.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CommunicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
